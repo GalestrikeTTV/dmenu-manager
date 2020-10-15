8fe7cdbbba8ba1e6a868b4a0bb3e7190ea7fb947
Tristan Zander <knavetnz@gmail.com>
2020-10-14 21:55:44 -0400

    Added dbus.rs for all dbus-related functions.

---
+++ mandwm/src/dbus.rs
@@ -0,0 +1,8 @@
+/// Given after mandwm.run() is called. It should give off events based on what's happening on the
+/// dbus.
+pub struct MandwmRunner {
+    pub should_run: bool,
+    // Some other dbus information we want to give
+}
+
+// TODO here put all the dbus initialization code
